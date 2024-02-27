860ea7281ca6776461213ae90e0401309fe6dc47
Maksim Fedorov <banana13373228@gmail.com>
2024-02-27 23:07:29 +0300

    photo add

--- src/bot.mjs
+++ src/bot.mjs
@@ -4,4 +4,6 @@ const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
 bot.on("text", msg => msg.reply.text(msg.text))
 
+bot.on("sendPhoto", msg => msg.reply.photo('https://www.meme-arsenal.com/memes/488f0194b841271f1e8601f4714d5969.jpg'))
+
 export default bot
